7289d5f2f41b67aa12be00f26fed87e79da5ef96
Benjamin K <k.saisoundar@gmail.com>
2020-01-07 18:01:59 +0100

    add chat/msg feature

--- README.md
+++ README.md
@@ -10,12 +10,26 @@ Implementation of the API server described in https://adrienjoly.com/cours-nodej
 $ curl http://localhost:3000/ # --> Hello World
 $ curl http://localhost:3000/hello # --> Quel est votre nom ?
 $ curl http://localhost:3000/hello?nom=Sasha # --> Bonjour, Sasha !
+$ curl -X POST --header "Content-Type: application/json" --data "{\"msg\":\"ville\"}" http://localhost:3000/chat # --> Nous sommes à Paris
+$ curl -X POST --header "Content-Type: application/json" --data "{\"msg\":\"météo\"}" http://localhost:3000/chat # --> Il fait beau
+
+# Depend on the content of the 'reponses.json' file
+# If you even reload the server, you can get the information if it is correctly stored in the 'reponses.json' file {
+
+# If 'demain' is not defined in 'reponses.json' file :
+$ curl -X POST --header "Content-Type: application/json" --data "{\"msg\":\"demain\"}" http://localhost:3000/chat # --> Je ne connais pas demain…
+# If you want to define 'demain' in 'reponses.json' file :
+$ curl -X POST --header "Content-Type: application/json" --data "{\"msg\":\"demain = Mercredi\"}" http://localhost:3000/chat # --> Merci pour cette information !
+# If 'demain' is defined in 'reponses.json' file : 
+$ curl -X POST --header "Content-Type: application/json" --data "{\"msg\":\"demain\"}" http://localhost:3000/chat # --> demain: Mercredi
+
+#}
 ```
 
 ## Setup
 
 ```
-$ git clone https://gitlab.eemi.tech/adrien.joly/node-1-2adev.git
+$ git clone https://github.com/Benjigo93/chatbot.git
 $ npm install
 $ npm start
 ```
--- server.js
+++ server.js
@@ -1,33 +1,78 @@
+// Require all the dependencies
+
 const express = require('express')
 const app = express()
 let encoding = require("encoding");
+let fs = require('fs')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/hello', (req, res)=>{
-  if(req.query.nom === undefined){
-    res.send('Quel est votre nom ?')
-  } else {
-    res.send('Bonjour, ' + req.query.nom + ' !' )
-  }
-
-})
-
+// Use json middleware to deal with json data
 app.use(express.json());
 
-app.post('/chat', (req, res)=>{
+
+// Define endpoints and responses
+// GET '/'
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+// GET '/hello'
+app.get('/hello', (req, res) => {
+
+  if (req.query.nom === undefined) {
+    res.send('Quel est votre nom ?')
+  } else {
+    res.send('Bonjour, ' + req.query.nom + ' !')
+  }
+})
+
+// POST '/chat'
+app.post('/chat', (req, res) => {
   let message = encoding.convert(req.body.msg, 'utf8', 'Latin_1');
-  console.log(req.body)
-  if(req.body.msg === "ville"){
-    res.send("Nous sommes à Paris");  
+  if (req.body.msg === "ville") {
+    res.send("Nous sommes à Paris");
   } else if (message == "mýtýo") {
-    res.send("Il fait beau"); 
+    res.send("Il fait beau");
+  } else if (req.body.msg !== undefined) {
+    let resultat = parseData(req.body.msg)
+    let JSONdata = getJSON()
+    if (req.body.msg.includes("=")) {
+      writeJSON(resultat)
+      res.send("Merci pour cette information !")
+    } else if (resultat[0] in JSONdata) {
+      res.send(resultat[0] + ":" + JSONdata[resultat[0]])
+    } else {
+      res.send("Je ne connais pas " + resultat[0] + "...")
+    }
   }
 });
 
-
+// Define PORT 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
-}) 
+})
+
+// Function to trim and to split the message which contents data for response.json and return it into an array
+function parseData(data) {
+  let message = data.split("=")
+  let array = message.map(s => s.trim());
+  return array
+}
+
+// Function to write into reponses.json
+function writeJSON(array) {
+  let rawdata = fs.readFileSync('reponses.json');
+  let jsonData = JSON.parse(rawdata);
+  let newJson = {}
+  newJson[array[0]] = array[1]
+  let finalJson = { ...jsonData, ...newJson }
+  let data = JSON.stringify(finalJson, null, 2);
+  fs.writeFileSync('reponses.json', data);
+}
+
+// Function to get data from reponses.json
+function getJSON() {
+  let rawdata = fs.readFileSync('reponses.json');
+  let reponses = JSON.parse(rawdata);
+  return reponses
+}
