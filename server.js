5b56c9b1a9da570f7052becd8baecf04ecad6952
Benjamin K <k.saisoundar@gmail.com>
2020-01-07 14:27:16 +0100

    add new dependance and modify server for exo 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+let encoding = require("encoding");
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -13,6 +14,19 @@ app.get('/hello', (req, res)=>{
   }
 
 })
+
+app.use(express.json());
+
+app.post('/chat', (req, res)=>{
+  let message = encoding.convert(req.body.msg, 'utf8', 'Latin_1');
+  console.log(req.body)
+  if(req.body.msg === "ville"){
+    res.send("Nous sommes à Paris");  
+  } else if (message == "mýtýo") {
+    res.send("Il fait beau"); 
+  }
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
