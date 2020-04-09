391006d7713411dbdb015af36e55d279a8bf9255
Benjamin Kichenamourty <k.saisoundar@gmail.com>
2020-04-10 00:14:20 +0200

    add exo 1.6

--- server.js
+++ server.js
@@ -1,76 +1,54 @@
-// Require all the dependencies
+const PORT = process.env.PORT || 3000;
+const FICHIER_REPONSES = "réponses.json";
 
-const express = require('express')
-const app = express()
-let fs = require('fs')
+const fs = require("fs");
+const express = require("express");
+const app = express();
 
-// Use json middleware to deal with json data
-app.use(express.json());
+console.log("Lecture des réponses depuis", FICHIER_REPONSES);
 
+fs.readFile(FICHIER_REPONSES, "utf8", (err, data) => {
+  if (err) throw err;
+  var reponses = JSON.parse(data);
 
-// Define endpoints and responses
-// GET '/'
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  app.use(express.json()); // to support JSON-encoded bodies
 
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
 
-// GET '/hello'
-app.get('/hello', (req, res) => {
-
-  if (req.query.nom === undefined) {
-    res.send('Quel est votre nom ?')
-  } else {
-    res.send('Bonjour, ' + req.query.nom + ' !')
-  }
-})
-
-// POST '/chat'
-app.post('/chat', (req, res) => {
-  if (req.body.msg === "ville") {
-    res.send("Nous sommes à Paris");
-  } else if (req.body.msg === "météo") {
-    res.send("Il fait beau");
-  } else if (req.body.msg !== undefined) {
-    let resultat = parseData(req.body.msg)
-    let JSONdata = getJSON()
-    if (req.body.msg.includes("=")) {
-      writeJSON(resultat)
-      res.send("Merci pour cette information !")
-    } else if (resultat[0] in JSONdata) {
-      res.send(resultat[0] + ":" + JSONdata[resultat[0]])
+  app.get("/hello", function (req, res) {
+    if (req.query.nom === undefined) {
+      res.send("Quel est votre nom ?");
     } else {
-      res.send("Je ne connais pas " + resultat[0] + "...")
+      res.send("Bonjour, " + req.query.nom + " !");
     }
-  }
+  });
+
+  app.post("/chat", function (req, res) {
+    if (req.body.msg === "ville") {
+      res.send("Nous sommes à Paris");
+    } else if (req.body.msg === "météo") {
+      res.send("Il fait beau");
+    } else if (req.body.msg.indexOf(" = ") !== -1) {
+      const [mot, definition] = req.body.msg.split(" = ");
+      reponses[mot] = definition;
+      fs.writeFile(FICHIER_REPONSES, JSON.stringify(reponses), (err) => {
+        if (err) {
+          res.send("Erreur... Reessayez plus tard.");
+        } else {
+          res.send("Merci pour cette information !");
+        }
+      });
+    } else if (reponses[req.body.msg] !== undefined) {
+      const definition = reponses[req.body.msg];
+      res.send(`${req.body.msg}: ${definition}`);
+    } else {
+      res.send(`Je ne connais pas ${req.body.msg}...`);
+    }
+  });
+
+  app.listen(PORT, function () {
+    console.log("The server is listening on port", PORT);
+  });
 });
-
-// Define PORT 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-// Function to trim and to split the message which contents data for response.json and return it into an array
-function parseData(data) {
-  let message = data.split("=")
-  let array = message.map(s => s.trim());
-  return array
-}
-
-// Function to write into reponses.json
-function writeJSON(array) {
-  let rawdata = fs.readFileSync('reponses.json');
-  let jsonData = JSON.parse(rawdata);
-  let newJson = {}
-  newJson[array[0]] = array[1]
-  let finalJson = { ...jsonData, ...newJson }
-  let data = JSON.stringify(finalJson, null, 2);
-  fs.writeFileSync('reponses.json', data);
-}
-
-// Function to get data from reponses.json
-function getJSON() {
-  let rawdata = fs.readFileSync('reponses.json');
-  let reponses = JSON.parse(rawdata);
-  return reponses
-}
