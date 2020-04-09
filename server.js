a0c569f940ff2648e1b1143bf7a5bd4cd4f198d1
Benjamin Kichenamourty <k.saisoundar@gmail.com>
2020-04-10 00:34:28 +0200

    add exo 1.8

--- server.js
+++ server.js
@@ -30,54 +30,55 @@ function writeFileAsPromised(file, data) {
 
 const promesseReponses = readFileAsPromised(FICHIER_REPONSES, "utf8");
 
-promesseReponses
-  .catch((err) => {
+(async () => {
+  let data
+  try {
+    data = await promesseReponses;
+  } catch (err) {
     console.error("echec de lecture du fichier:", err);
     process.exit();
-  })
-  .then((data) => {
-    var reponses = JSON.parse(data);
+  }
 
-    app.use(express.json()); // to support JSON-encoded bodies
+  var reponses = JSON.parse(data);
 
-    app.get("/", function (req, res) {
-      res.send("Hello World");
-    });
+  app.use(express.json()); // to support JSON-encoded bodies
 
-    app.get("/hello", function (req, res) {
-      if (req.query.nom === undefined) {
-        res.send("Quel est votre nom ?");
-      } else {
-        res.send("Bonjour, " + req.query.nom + " !");
-      }
-    });
-
-    app.post("/chat", function (req, res) {
-      if (req.body.msg === "ville") {
-        res.send("Nous sommes à Paris");
-      } else if (req.body.msg === "météo") {
-        res.send("Il fait beau");
-      } else if (req.body.msg.indexOf(" = ") !== -1) {
-        const [mot, definition] = req.body.msg.split(" = ");
-        reponses[mot] = definition;
-
-        writeFileAsPromised(FICHIER_REPONSES, JSON.stringify(reponses))
-          .then(() => {
-            res.send("Merci pour cette information !");
-          })
-          .catch((err) => {
-            console.error(err);
-            res.send("Erreur... Reessayez plus tard.");
-          });
-      } else if (reponses[req.body.msg] !== undefined) {
-        const definition = reponses[req.body.msg];
-        res.send(`${req.body.msg}: ${definition}`);
-      } else {
-        res.send(`Je ne connais pas ${req.body.msg}...`);
-      }
-    });
-
-    app.listen(PORT, function () {
-      console.log("The server is listening on port", PORT);
-    });
+  app.get("/", function (req, res) {
+    res.send("Hello World");
   });
+
+  app.get("/hello", function (req, res) {
+    if (req.query.nom === undefined) {
+      res.send("Quel est votre nom ?");
+    } else {
+      res.send("Bonjour, " + req.query.nom + " !");
+    }
+  });
+
+  app.post("/chat", async function (req, res) {
+    if (req.body.msg === "ville") {
+      res.send("Nous sommes à Paris");
+    } else if (req.body.msg === "météo") {
+      res.send("Il fait beau");
+    } else if (req.body.msg.indexOf(" = ") !== -1) {
+      const [mot, definition] = req.body.msg.split(" = ");
+      reponses[mot] = definition;
+      try {
+        await writeFileAsPromised(FICHIER_REPONSES, JSON.stringify(reponses))
+        res.send("Merci pour cette information !");
+      } catch (err) {
+        console.error(err);
+        res.send("Erreur... Reessayez plus tard.");
+      }
+    } else if (reponses[req.body.msg] !== undefined) {
+      const definition = reponses[req.body.msg];
+      res.send(`${req.body.msg}: ${definition}`);
+    } else {
+      res.send(`Je ne connais pas ${req.body.msg}...`);
+    }
+  });
+
+  app.listen(PORT, function () {
+    console.log("The server is listening on port", PORT);
+  });
+})();
