73cca3494e38729d8080fe3b4777aa815e56ea0f
Benjamin Kichenamourty <k.saisoundar@gmail.com>
2020-04-10 02:05:56 +0200

    add dates.js - exo 2.1

---
+++ dates.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+(async function() {
+  // Connection URL
+  const uri = "mongodb+srv://chatbot:chatbot@chatbot-nodejs-wts3i.azure.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  // Database Name
+  const dbName = 'chatbot';
+
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+
+    const db = client.db(dbName);
+    const col = db.collection('dates')
+
+    // Add new date
+    let add = await col.insertOne({date: new Date()})
+    assert.equal(1, add.insertedCount)
+
+    // Show dates
+    const dates = await col.find({}).toArray();
+    console.log(dates)
+
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  client.close();
+})();
